02fc27042c93dafbd7c108b864409d92de2df0ab
Javier Palomares <javier.palomares.90@gmail.com>
2019-03-24 17:54:29 -0500

    Convert tabs to space

--- dumbbell_topo.py
+++ dumbbell_topo.py
@@ -13,58 +13,57 @@ MEDIUM = "81ms"
 LONG = "162ms"
 
 class Dumbbell(Topo):
-	"Dumbbell for TCP Congestion Control"
     def __init__(self,prop_delay):
         super(Dumbbell,self).__init__()
-		self.prop_delay = prop_delay
+        self.prop_delay = prop_delay
 
-	def build(self):
-		#left side
-		# access and backbone switches
-		access_router_1 = self.addSwitch('s1')
-		backbone_router_1 = self.addSwitch('s2')
-		# the 2 sources
-		source_1 = self.addHost('h1')
-		source_2 = self.addHost('h2')
-		# add link from sources to access switch
-		self.addLink(source_1,access_router_1)
-		self.addLink(source_2,access_router_1)
-		# add link from access to back switch
-		self.addLink(access_router_1,backbone_router_1)
+    def build(self):
+        #left side
+        # access and backbone switches
+        access_router_1 = self.addSwitch('s1')
+        backbone_router_1 = self.addSwitch('s2')
+        # the 2 sources
+        source_1 = self.addHost('h1')
+        source_2 = self.addHost('h2')
+        # add link from sources to access switch
+        self.addLink(source_1,access_router_1)
+        self.addLink(source_2,access_router_1)
+        # add link from access to back switch
+        self.addLink(access_router_1,backbone_router_1)
 
-		# right side
+        # right side
 
-		# access and backbone switches
-		access_router_2 = self.addSwitch('s3')
-		backbone_router_2 = self.addSwitch('s4')
-		# the 2 sources
-		receiver_1 = self.addHost('h3')
-		receiver_2 = self.addHost('h4')
-		# add link from sources to access switch
-		self.addLink(receiver_1, access_router_2)
-		self.addLink(receiver_2, access_router_2)
-		# add link from access to back switch
-		self.addLink(access_router_2,backbone_router_2)
-		
-		# add link between two backbone routers
-		# use the propagation delay provided at construction
-		self.addLink(backbone_router_1,backbone_router_2,delay=self.prop_delay)
+        # access and backbone switches
+        access_router_2 = self.addSwitch('s3')
+        backbone_router_2 = self.addSwitch('s4')
+        # the 2 sources
+        receiver_1 = self.addHost('h3')
+        receiver_2 = self.addHost('h4')
+        # add link from sources to access switch
+        self.addLink(receiver_1, access_router_2)
+        self.addLink(receiver_2, access_router_2)
+        # add link from access to back switch
+        self.addLink(access_router_2,backbone_router_2)
+
+        # add link between two backbone routers
+        # use the propagation delay provided at construction
+        self.addLink(backbone_router_1,backbone_router_2,delay=self.prop_delay)
 
 
 def simple_test():
-	"Create and test a dumbell network"
-	dumbbell = Dumbbell(SHORT)
-	net = Mininet(dumbbell)
-	net.start()
-	print("Dumping host connections")
-	dumpNodeConnections(net.hosts)
-	print("Testing network connectivity")
-	net.pingAll()
-	net.stop()
+    "Create and test a dumbell network"
+    dumbbell = Dumbbell(SHORT)
+    net = Mininet(dumbbell)
+    net.start()
+    print("Dumping host connections")
+    dumpNodeConnections(net.hosts)
+    print("Testing network connectivity")
+    net.pingAll()
+    net.stop()
 
 if __name__ =='__main__':
-	setLogLevel('info')
-	simple_test()
-		
+    setLogLevel('info')
+    simple_test()
+
 
 
